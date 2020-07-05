29a7f0f23e50a4cfa35e0118b7e5749942bab563
jason-nordheim <jason.nordheim@gmail.com>
2020-07-05 15:13:17 -0600

    adding changes thus far

--- server.js
+++ server.js
@@ -24,15 +24,57 @@ const SESSION_CONFIG = {
     secure: IN_PROD,
   },
 } 
+
+const users = [
+  { id: 1, name: 'Jason', email: 'jason@gmail.com', password: 'secretPassword' },
+  { id: 2, name: 'Sam', email: 'sam@gmail.com', password: 'secretPassword' },
+  { id: 3, name: 'Susan', email: 'susan@gmail.com', password: 'secretPassword' },
+  { id: 4, name: 'Larry', email: 'larry@gmail.com', password: 'secretPassword' },
+]
+
 const app = express() 
 app.use(session(SESSION_CONFIG))
 
 
-app.get('/', () => {})
-app.get('/login', () => {})
-app.post('/login', ()=> {})
-app.get('/register', () => {})
-app.post('/register', ()=>{})
-app.post('/logout', ()=>{})
+app.get('/', (req, res) => {
+  const userId  = 1 // req.session  
+  res.send(`
+    <h1>Welcome</h1>
+    ${ userId
+        ? `    
+        <a href='/home'>Home</a> 
+        <form method="post" action="/logout">
+        <button>Logout</button>`
+        : `
+        <a href='/login'>Login</a> 
+        <a href='/register'>Register</a>`
+    }
+    </form> 
+  `);
+})
+app.get('/home', (req, res) => {
+  res.send(`
+    <h1>Home</h1> 
+    <a href="/">Main</a> 
+    <ul>
+      <li>Name: </li>
+      <li>Email: </li> 
+    </ul>
+  `)
+})
+app.get('/login', (req, res) => {
+  res.send(`
+    <h1>Login</h1>
+    <form method="post" action"/login">
+      <input type="email" name="email" placeholder="email" require /> 
+      <input type="password" name="email" placeholder="email" require /> 
+    </form>
+  `)
+
+})
+app.post('/login', (req, res)=> {})
+app.get('/register', (req, res) => {})
+app.post('/register', (req, res)=>{})
+app.post('/logout', (req, res)=>{})
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`))
